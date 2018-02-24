08553f33f69abaf080a747c27916f988c964b0ba
Max Segal <max.segal16@gmail.com>
2018-02-23 20:18:19 -0800

    Add files via upload

--- regi_form.php
+++ regi_form.php
@@ -1,26 +1,14 @@
 <?php 
-  $db = new mysqli('localhost','octo','w3b7ysX6','octo');
+  $db = mysqli_connect('localhost','octo','w3b7ysX6','octo');
   or die("Error");
   echo "sup";
   
   if (($_POST['password']) == ($_POST['confirmpassword'])){
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+  
+    mysqli_query($db, "INSERT INTO users  VALUES ('$_POST['username']', '$_POST['password']', '$_POST['firstName']', '$_POST['lastName']', '$_POST['email']')");
+    
+    echo "INSERTED!"
 
-    echo $firstName.$lastName.$username.$email.$password;
-    
-    $stmt = $db->prepare("INSERT INTO users  VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param($username, $password, $firstName, $lastName, $email)
-    
-    $result = $stmt->execute();
-    if ($result) {
-	    echo "User registered successfully";
-    } else {
-	      echo "Error: " . $query . "<br>" . $db->error;
-    }
   }
 $db->close();
 ?>
