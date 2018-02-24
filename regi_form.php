05674ceb8e3db03df23864ab56bb15fac27005d9
Max Segal <max.segal16@gmail.com>
2018-02-23 19:28:16 -0800

    trying to fix insert into db

--- regi_form.php
+++ regi_form.php
@@ -1,33 +1,26 @@
 <?php 
+  $db = new mysqli('localhost','octo','w3b7ysX6','octo');
+  or die("Error");
+  echo "sup";
+  
+  if (($_POST['password']) == ($_POST['confirmpassword'])){
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$db = new mysqli('localhost','octo','w3b7ysX6','octo');
-or die("Error");
-echo "sup";
-
-
-
-if (($_POST['password']) == ($_POST['confirmpassword'])){
-
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-echo $firstName.$lastName.$username.$email.$password;
-
-$query = "INSERT INTO users  VALUES ($username, $password, $firstName, $lastName, $email)";
-
-if ($db->query($query) === TRUE) {
-	echo "User registered successfully";
-} else {
-	echo "Error: " . $query . "<br>" . $db->error;
-}
-
-}
-
+    echo $firstName.$lastName.$username.$email.$password;
+    
+    $stmt = $db->prepare("INSERT INTO users  VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param($username, $password, $firstName, $lastName, $email)
+    
+    $result = $stmt->execute();
+    if ($result) {
+	    echo "User registered successfully";
+    } else {
+	      echo "Error: " . $query . "<br>" . $db->error;
+    }
+  }
 $db->close();
-
-
-
 ?>
