f2d91e384c41ea2765b2a99413b35675dad43f3d
camcamchong <cameronleechong@gmail.com>
2018-02-23 18:49:01 -0800

    Add files via upload

--- regi_form.php
+++ regi_form.php
@@ -1,25 +1,27 @@
 <?php 
 
-$db=mysqli_connect(“localhost”, “octo”, “w3b7ysX6”)
-or die(“Error”);
-echo ”sup”;
+$db = new mysqli('localhost','octo','w3b7ysX6','octo');
+or die("Error");
+echo "sup";
 
 
 
-if (($_POST[‘password’]) == ($_POST[‘confirmpassword])){
+if (($_POST['password']) == ($_POST['confirmpassword'])){
 
-$firstName = $_POST[‘firstName’];
-$lastName = $_POST[‘lastName’];
-$username = $_POST[‘username’];
-$email = $_POST[‘email’];
-$password = $_POST[‘password’];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-$query = “INSERT INTO ‘users’ (username, password, firstname, lastname, email) VALUES (‘$username’, ‘$password’, ‘$firstName’, ‘$lastName’, ‘$email’);
+echo $firstName.$lastName.$username.$email.$password;
+
+$query = "INSERT INTO users  VALUES ($username, $password, $firstName, $lastName, $email)";
 
 if ($db->query($query) === TRUE) {
-	echo “User registered successfully”;
+	echo "User registered successfully";
 } else {
-	echo “Error: “ . $query . “<br>” . $db->error;
+	echo "Error: " . $query . "<br>" . $db->error;
 }
 
 }
