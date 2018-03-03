ca49b457bb31ae4393de0edd1754d19430422531
camcamchong <cameronleechong@gmail.com>
2018-03-03 13:01:20 -0800

    Update addExpense.php

--- addExpense.php
+++ addExpense.php
@@ -1,24 +1,49 @@
+<?php
+$con = new mysqli('localhost','root',"",'cosc310');
+	if($con ->connect_error){
+    die("Connection failed". $con->connect_error);}
+
+$years = array();
+$year = 2017;
+if($stmt=$con->prepare("SELECT DISTINCT year FROM finances WHERE username = ?")){
+$stmt->bind_param("s",$username);
+$username='test';
+//set username to session attribute
+$stmt->execute();
+$stmt->bind_result($a);
+while($stmt->fetch()){
+	//echo $a;
+array_push($years,$a);
+}
+$stmt->close();
+// for($i = 0; $i < count($years); $i++){
+// 	echo $years[$i];
+// }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Add Expense</title>
+	<title>Add Expense To a Month</title>
 </head>
 <body>
 
 
 
-	<form method = "post" action = "selectExpense.php">
-   
- 	 <input type="text" name="firstname" placeholder="Enter $ Amount Here" >
+	<form method = "post" action = "processExpense.php">
+
+ 	 <input type="text" name="amount" placeholder="Enter $ Amount Here" >
   <br>
-   <select>
+   <select name ="category">
    <option disabled>Choose Expense</option>
-  <option >Food</option>
-  <option >Rent</option>
-  <option >Miscellaneous</option>
+  <option >food</option>
+  <option >rent</option>
+  <option >clothing</option>
+	<option>entertainment</option>
+	<option>income</option>
 </select>
   <br>
-<select>
+<select name = "month">
    <option disabled>Choose Month</option>
   <option >January</option>
   <option >February</option>
@@ -34,8 +59,19 @@
   <option >December</option>
 </select>
   <br>
+	<select name = "year">
+	<?php
+	for($i = 0; $i < count($years); $i++){
+		echo '<option>'.$years[$i].'</option>';
+	}
+		?>
+	</select>
   <input type="submit" value="Submit">
-	</form> 
-
+	</form>
+	<?php
+	$msg = "";
+	$msg=$_GET['message'];
+	echo $msg;
+	?>
 </body>
 </html>
