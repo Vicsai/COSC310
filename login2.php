90e39a9071b6249f9b3212c37b0aec1335cb5296
Vicsai <victsai1221@gmail.com>
2018-03-05 19:40:24 -0800

    converted html to php

--- login2.php
+++ login2.php
@@ -13,22 +13,21 @@ else{
 </head>
 <body>
 	<?php
-	if(isset($_POST["submit"])){
+	print("i work");
 	$username=mysqli_real_escape_string($con,$_POST['username']);
 	$password=mysqli_real_escape_string($con,$_POST['password']);
 	$sql="SELECT username FROM users WHERE username='$username' AND password='$password'";
 	$result=$con->query($sql);
 	$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 	$count=mysqli_num_rows($result);
-
+	print ("i am working");
 	if($count==1){
 		$_SESSION['currentUser']=$username;
-		header("location: welcome.php");
+		header("location: UserPage.php");
 		print("logged in");
 	}
 	else
 		print("Your login or password is invalid");
-	}
 	$con->close();
 	?>
 </body>
