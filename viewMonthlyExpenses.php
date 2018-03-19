fb2035a4d479c1b50a71754ff9521399b38b1a55
Vicsai <victsai1221@gmail.com>
2018-03-18 17:56:52 -0700

    added savings

--- viewMonthlyExpenses.php
+++ viewMonthlyExpenses.php
@@ -2,19 +2,14 @@
 <?php
 
 $con = new mysqli('localhost','octo','w3b7ysX6','octo');
-//
-//
-//
-// if($con ->connect_error){
-//     die("Connection failed". $con->connect_error);
-// }
-//
-// echo 'Connected Succesfully';
-
-//$con = new mysqli('localhost','root',"",'cosc310');
-
 if($con ->connect_error){
     die("Connection failed". $con->connect_error);
+}
+session_start();
+$isLoggedIn = false;
+if(isset($_SESSION['user'])){
+$isLoggedIn =true;
+$u = $_SESSION['user'];
 }
 
 
